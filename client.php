9ad295d7bf2bd0fd31559d9229339412aa16812d
Cesar Schneider <cesschneider@gmail.com>
2013-07-12 21:04:51 -0300

    Updated video to final version, fixed email formating

--- client.php
+++ client.php
@@ -1,7 +1,5 @@
 <?php
 	
-	require("phpmailer.php");
-	
   $date = date('r');
   $remote_addr = $_SERVER['REMOTE_ADDR'];
 	$nome = $_POST['name'];
@@ -9,13 +7,11 @@
 	$email = $_POST['email'];
 	$mensagem = $_POST['message'];
 	
-	
 	$data = "$date;$remote_addr;$nome;$tel;$email;$mensagem;".PHP_EOL;
 	
 	$fh = fopen("clientes.txt", "a");
 	fwrite($fh, $data);
 	fclose($fh);
-	
 	
 	//email-------------------------------------------------------------------------
 	$corpo_email = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -25,15 +21,15 @@
 		<tbody>
 		<tr>
 			<td style="padding-right:15px;">
-				<img width="128" height="142" src="imgs/header_logo.png">
+				<img width="128" height="142" src="http://qfixr.com.br/imgs/header_logo.png">
 			</td>
 			<td colspan="2" style="color:#686868;font:20px Arial">
-				Parabéns! 
+				Parab&eacute;ns! 
 				<div style="color:#333;font:16px Arial;vertical-align:top;padding-top:10px">
-					Agora você é um cliente Qfixr. Estaremos entrando em contato com você o mais breve possível.
+					Agora voc&ecirc; &eacute; um cliente Qfixr. Entraremos em contato com voc&ecirc; o mais breve poss&iacute;vel.
 				</div>
 				<div style="color:#333;font:24px Arial;vertical-align:top;padding-top:24px" align="left">
-					<img  src="imgs/client_phone_icon.png" height="30"/>&nbsp;4063-8100
+					<img  src="http://qfixr.com.br/imgs/client_phone_icon.png" height="30"/>&nbsp;4063-8100
 				</div>
 			</td>
 		</tr>
@@ -44,33 +40,11 @@
 	
 	//===========================================================================
 	
-	$to = $nome;
-	$email = $email;
-	$fromaddress = 'contato@qfixr.com.br';
-	$fromname = 'Qfixr Brasil';
+	$headers = "Content-Type: text/html; charset=utf-8\r\n";
+	$headers = "MIME-Version: 1.0\r\n";
 	
-	$mail = new PHPMailer();
-	
-	$mail->From     = $fromaddress;
-	$mail->FromName = $fromname;
-	$mail->AddAddress($email,$nome);
-	
-	$mail->WordWrap = 50;
-	$mail->IsHTML(true);
-	
-	$mail->Subject  =  "Bem-vindo ao QFIXR!";
-	$mail->Body     =  $corpo_email;//E_MENSAGEM." <bold>$idvela</bold> e do email $email.";
-	$mail->AltBody  =  $corpo_email;//E_MENSAGEM." <bold>$idvela</bold> e do email $email.";
-	
-	if(!$mail->Send()) {
-		$recipient = "contato@qfixr.com.br";
-		$subject = 'Envio Falhou';
-		$content = $body;	
-		mail($recipient, $subject, $content, "From: contato@qfixr.com.br\r\nReply-To: $email\r\nX-Mailer: DT_formmail");
-		exit;
-	}
-
-
+	mail('atendimento@qfixr.com.br', 'Solicitação de atendimento', str_replace(';', "<br>", $body), $headers);
+	mail($_POST['email'], 'Qfixr | Confirmação de cadastro', $corpo_email, $headers);
 	
 	echo "Gravado";
 	
--- technician.php
+++ technician.php
@@ -8,13 +8,11 @@
 	$tel = $_POST['tel'];
 	$email = $_POST['email'];
 	
-	
 	$data = "$date;$remote_addr;$nome;$tel;$email;".PHP_EOL;
 	
 	$fh = fopen("tecnicos.txt", "a");
 	fwrite($fh, $data);
 	fclose($fh);
-	
 	
 	//email-------------------------------------------------------------------------
 	$corpo_email = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -24,15 +22,15 @@
 		<tbody>
 		<tr>
 			<td style="padding-right:15px;">
-				<img width="128" height="142" src="imgs/header_logo.png">
+				<img width="128" height="142" src="http://qfixr.com.br/imgs/header_logo.png">
 			</td>
 			<td colspan="2" style="color:#686868;font:20px Arial">
 				Cadastro realizado com sucesso! 
 				<div style="color:#333;font:16px Arial;vertical-align:top;padding-top:10px">
-					Agora você está mais perto de ser um técnico Qfixr. Aguarde, pois logo entraremos em contato. 
+					Agora voc&ecirc; est&aacute; mais perto de ser um t&eacute;cnico Qfixr. Aguarde, pois logo entraremos em contato. 
 				</div>
 				<div style="color:#333;font:24px Arial;vertical-align:top;padding-top:24px" align="left">
-					<img  src="imgs/client_phone_icon.png" height="30"/>&nbsp;4063-8100
+					<img  src="http://qfixr.com.br/imgs/client_phone_icon.png" height="30"/>&nbsp;4063-8100
 				</div>
 			</td>
 		</tr>
@@ -43,31 +41,11 @@
 	
 	//===========================================================================
 	
-	$to = $nome;
-	$email = $email;
-	$fromaddress = 'contato@qfixr.com.br';
-	$fromname = 'Qfixr Brasil';
+	$headers = "Content-Type: text/html; charset=utf-8\r\n";
+	$headers = "MIME-Version: 1.0\r\n";
 	
-	$mail = new PHPMailer();
-	
-	$mail->From     = $fromaddress;
-	$mail->FromName = $fromname;
-	$mail->AddAddress($email,$nome);
-	
-	$mail->WordWrap = 50;
-	$mail->IsHTML(true);
-	
-	$mail->Subject  =  "Bem-vindo ao QFIXR!";
-	$mail->Body     =  $corpo_email;//E_MENSAGEM." <bold>$idvela</bold> e do email $email.";
-	$mail->AltBody  =  $corpo_email;//E_MENSAGEM." <bold>$idvela</bold> e do email $email.";
-	
-	if(!$mail->Send()) {
-		$recipient = "contato@qfixr.com.br";
-		$subject = 'Envio Falhou';
-		$content = $body;	
-		mail($recipient, $subject, $content, "From: contato@qfixr.com.br\r\nReply-To: $email\r\nX-Mailer: DT_formmail");
-		exit;
-	}
+	mail('contato@qfixr.com.br', 'Qualificação de técnicos', str_replace(';', "<br>", $body), $headers);
+	mail($_POST['email'], 'Qfixr | Confirmação de cadastro', $corpo_email, $headers);
 	
 	echo "Gravado";
 	
