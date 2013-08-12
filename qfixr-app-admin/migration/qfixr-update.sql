da1aa8a25e982088c87b3b72ade05acc985b204a
Cesar Mascarenhas <cesarmascarenhas@gmail.com>
2013-08-12 14:57:54 -0300

    migration folder with new table

---
+++ qfixr-app-admin/migration/qfixr-update.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tempo de Geração: 
+-- Versão do Servidor: 5.5.24-log
+-- Versão do PHP: 5.4.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Banco de Dados: `qfixr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `andamento`
+--
+
+CREATE TABLE IF NOT EXISTS `andamento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_chamado` int(11) NOT NULL,
+  `id_tecnico` int(11) NOT NULL,
+  `descricao` tinytext NOT NULL,
+  `criado_em` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
